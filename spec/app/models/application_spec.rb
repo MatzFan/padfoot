897945e5388b30fb05b1a7b709e6058fddce0533
Bruce Steedman <bruce.steedman@gmail.com>
2014-12-09 17:30:31 +0000

    Test application creation with fk's

--- spec/app/models/application_spec.rb
+++ spec/app/models/application_spec.rb
@@ -8,21 +8,22 @@ describe 'Application Model' do
     ParishAlias.create(name: 'St Helier')
   end
 
+  let(:pk) { {app_ref: 'P/2012/0219'} }
   let(:fk) { {app_status: 'Pending', app_category: 'P', app_officer: 'A Other',
              app_parish: 'St Helier', app_agent: 'Riva Architects'}
            }
 
-  let(:application) { Application.create(app_ref: 'P/2012/0219') }
+  let(:app_no_fks) { Application.create(pk) }
+  let(:application) { Application.create(pk.merge(fk)) }
 
   describe "without fk's" do
     it 'can be created' do
-      expect(application).not_to be_nil
+      expect(app_no_fks).not_to be_nil
     end
   end
 
   describe "with fk's" do
     it 'can be created' do
-
       expect(application).not_to be_nil
     end
   end
