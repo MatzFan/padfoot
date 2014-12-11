a0e1177593420880f195ab36561c8fd51da495ab
Bruce Steedman <bruce.steedman@gmail.com>
2014-12-11 12:35:18 +0000

    Add users controller with new route. Add registration form.

--- app/controllers/page.rb
+++ app/controllers/page.rb
@@ -1,15 +1,15 @@
 Padfoot::App.controllers :page do
 
   get :about, map: '/about' do
-    render :haml, 'about'
+    render :about
   end
 
   get :contact , map: '/contact' do
-    render :haml, 'contact'
+    render :contact
   end
 
   get :home, map: '/' do
-    render :haml, 'home'
+    render :home
   end
 
 end
