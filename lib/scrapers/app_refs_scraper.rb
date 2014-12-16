b043080fcaa44c7ddeb9ab4867560d2afae8439e
Bruce Steedman <bruce.steedman@gmail.com>
2014-12-16 22:09:16 +0000

    app_ref_scraper now returns app_refs for a year since a given ref #

--- lib/scrapers/app_refs_scraper.rb
+++ lib/scrapers/app_refs_scraper.rb
@@ -1,5 +1,6 @@
-class AppRefsScraper # scrapes app refs for a given year
+require 'json'
 
+class AppRefsScraper # scrapes app refs for a given year
 
   ROOT = 'https://www.mygov.je/'
   CURL = 'curl -s -X POST -H "Content-Type: application/json" -d'
@@ -9,43 +10,60 @@ class AppRefsScraper # scrapes app refs for a given year
   SEARCH = '"SearchParameters":'
   REQ_PAGE = '_layouts/15/PlanningAjaxServices/PlanningSearch.svc/Search'
   RESULT = 'ResultHTML'
-  DELIM = 'href="https://www.mygov.je//Planning/Pages/PlanningApplicationDetail.aspx?s=1&amp;r='
+  HEADER = 'HeaderHTML'
+  DETAILS_URL = '/Planning/Pages/PlanningApplicationDetail.aspx?s=1&amp;r='
 
-  attr_reader :year, :page_num
+  attr_reader :year
 
-  def initialize(year)
+  def initialize(year:) # Ruby 2.1 required keyword arg
     @year = year.to_s
-    @page_num = 1
   end
 
-  def app_refs_on_page(page_num)
-    app_refs_array(page_num).map { |e| e.split('">')[0] }.uniq
+  def app_refs_from(num_string: '0000')
+    refs_arr = []
+    (1..3).collect do |page_num|
+      refs_arr += app_refs_on_page(page_num)
+      return refs_arr if refs_arr.any? { |ref| ref =~ /\/#{num_string}/ }
+    end
   end
 
-  def app_refs_array(page_num)
-    source = json_for_page(page_num)
-    app_arr = JSON.parse(source)[RESULT].split(DELIM)[1..-1]
+  def app_refs_on_page(page)
+    app_refs_array(page).map { |e| e.split('">')[0] }.uniq
+  end
+
+  def num_apps # returns total number for the year
+    JSON.parse(json_for_page(1))[HEADER].split[8].to_i
+  end
+
+  def delimiter
+    'href="' + ROOT + DETAILS_URL
+  end
+
+  def app_refs_array(page)
+    JSON.parse(json_for_page(page))[RESULT].split(delimiter)[1..-1]
   end
 
   def url_params
     URL + SITE
   end
 
-  def common_params
-    COMMON + '"|05|' + page_num.to_s + '||||"'
+  def common_params(page)
+    COMMON + '"|05|' + page.to_s + '||||"'
   end
 
   def search_params
     SEARCH + '"|1301||||0|All|All|01|01|' + year + '|' + '31|12|' + year + '"'
   end
 
-  def params
-    url_params + ',' + common_params + ',' + search_params
+  def params(page)
+    url_params + ',' + common_params(page) + ',' + search_params
   end
 
-  def json_for_page(page_num)
-    cmd = CURL + " {'" + params + "}' " + ROOT + REQ_PAGE
+  def json_for_page(page)
+    cmd = CURL + " {'" + params(page) + "}' " + ROOT + REQ_PAGE
     `#{cmd}`
   end
 
 end
+
+# puts AppRefsScraper.new(year: 2013).app_refs_array 2
--- spec/lib/app_refs_scraper_spec.rb
+++ spec/lib/app_refs_scraper_spec.rb
@@ -1,16 +1,16 @@
 describe AppRefsScraper do
 
-  let(:scraper) { AppRefsScraper.new(2013) }
-  let(:page1_2013) { %w(P/2013/1848 P/2013/1847 P/2013/1846 P/2013/1843
-                        P/2013/1842 A/2013/1845 A/2013/1844 RC/2013/1841
-                        P/2013/1840 P/2013/1836) }
-
-  it '#initialize' do
-    expect(scraper).not_to be_nil
-  end
+  let(:scraper) { AppRefsScraper.new(year: 2013) }
+  let(:page3_2013) { %w(P/2013/1812 P/2013/1813 P/2013/1814 P/2013/1815
+                        P/2013/1816 P/2013/1817 P/2013/1820 P/2013/1821
+                        P/2013/1822 P/2013/1823) }
 
   it '#json_for_page' do
     expect(scraper.json_for_page(1)).to include('Scroll down to view')
+  end
+
+  it '#num_apps' do
+    expect(scraper.num_apps).to eq(1447)
   end
 
   it '#app_refs_array' do
@@ -18,7 +18,11 @@ describe AppRefsScraper do
   end
 
   it '#app_refs_on_page' do
-    expect(scraper.app_refs_on_page(1)).to eq(page1_2013)
+    expect(scraper.app_refs_on_page(3)).to match_array(page3_2013)
+  end
+
+  it '#app_refs_from' do
+    expect(scraper.app_refs_from(num_string: '1833').count).to eq(20)
   end
 
 end
