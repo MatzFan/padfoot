02e4fc69edfcdba0c01b6f2af63e9aa06041f459
Bruce Steedman <bruce.steedman@gmail.com>
2014-12-19 18:47:21 +0000

    Details scraper now returns application details data

--- lib/scrapers/app_details_scraper.rb
+++ lib/scrapers/app_details_scraper.rb
@@ -4,23 +4,57 @@ class AppDetailsScraper
 
   ROOT = 'https://www.mygov.je/'
   DETAILS_PAGE = '/Planning/Pages/PlanningApplicationDetail.aspx?s=1&r='
+  TABLE_CSS = ".//table[@class='pln-searchd-table']"
+  DETAILS_TABLE_TITLES = ["Reference",
+                          "Category",
+                          "Status",
+                          "Officer",
+                          "Applicant",
+                          "Description",
+                          "ApplicationAddress",
+                          "RoadName",
+                          "Parish",
+                          "PostCode",
+                          "Constraints",
+                          "Agent"]
+  ID_DELIM = 'ctl00_lbl'
 
-  attr_reader :agent, :app_ref, :details_title
+  attr_reader :agent, :app_ref, :details_page
 
   def initialize(app_ref)
     @agent = Mechanize.new
     @app_ref = app_ref
     @details_page = details_page
+    return unless valid_details_page?
     @details_source = details_page.body
   end
 
-  def details_title
-    @details_title = details_page.title
+  def valid_details_page?
+    details_page.title.include?('Planning Application Detail')
   end
 
   def details_page
-    # agent.agent.http.ssl_version = :TLSv1 # Lord knows why this needs to be set
     agent.get(ROOT + DETAILS_PAGE + app_ref)
+  end
+
+  def details_data
+    (0..1).map { |n| details_table(n).map { |i| i.text } }.flatten if t_valid?
+  end
+
+  def t_valid? # valid details table titles
+    details_table_titles == DETAILS_TABLE_TITLES
+  end
+
+  def details_table(n) # app details are split over 2 tables with same class
+    details_page.search(TABLE_CSS)[n].css('tr').css('td').css('span')
+  end
+
+  def details_table_titles
+    (0..1).map { |n| details_table(n).map { |i| parse(i.attr('id')) } }.flatten
+  end
+
+  def parse(text)
+    text.split(ID_DELIM).last
   end
 
 end
--- spec/lib/scrapers/app_details_scraper.spec.rb
+++ spec/lib/scrapers/app_details_scraper.spec.rb
@@ -1,10 +1,30 @@
 describe AppDetailsScraper do
 
-  let(:scraper) { AppDetailsScraper.new('P/2014/2191') }
+  let(:scraper) { AppDetailsScraper.new('RW/2014/0548') }
+  let(:details) { ["RW/2014/0548",
+                   "RW",
+                   "APPEAL",
+                   "Richard Greig",
+                   "Mr & Mrs R.I.G. Hardcastle, Le Mont Sohier, St. Brelade, JE3 8EA",
+                   "Replace 5 No. windows on South elevation..... REQUEST FOR RECONSIDERATION for refusal of planning permission.",
+                   "Homewood",
+                   "Le Mont Sohier",
+                   "St. Brelade",
+                   "JE3 8EA",
+                   "Built-Up Area, Green Backdrop Zone, Potential Listed Building, Primary Route Network",
+                   ""
+                   ] }
 
-  it '#initialize' do
-    expect(scraper.details_title).to include('Planning Application Detail')
+  xit '#valid_details_page?' do
+    expect(scraper.valid_details_page?).to be_truthy
   end
 
+  xit '#t_valid?' do
+    expect(scraper.t_valid?).to be_truthy
+  end
+
+  it '#details_data' do
+    expect(scraper.details_data).to eq(details)
+  end
 
 end
