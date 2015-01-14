feb7461448dfcacc869f3712209551af1f2eeeb0
Bruce Steedman <bruce.steedman@gmail.com>
2015-01-14 13:50:08 +0000

    Change unicorn config to disconnect in before hook, modify planning_app/index view

--- app/controllers/planning_app.rb
+++ app/controllers/planning_app.rb
@@ -3,7 +3,7 @@ Padfoot::App.controllers :planning_app do
   before { redirect('/login') unless signed_in? }
 
   get :index, map: '/applications/index' do
-    columns = [:order, :app_ref, :app_category, :app_status, :app_agent, :app_address, :app_description]
+    columns = [:order, :app_ref, :app_code, :app_status, :app_agent, :app_address, :app_description]
     @titles = columns.map { |c| c.to_s.split('_').last.capitalize }
     @apps = PlanningApp.select_map(columns)
     render :index
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,3 +1,12 @@
-worker_processes 3
+worker_processes Integer(ENV['WEB_CONCURRENCY'] || 3)
 timeout 15
 preload_app true
+
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(Sequel) and Sequel.connection.disconnect # essential as PG connection persists
+end
