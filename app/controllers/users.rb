b974cedf3f6136442fd67f65917188affad94830
Bruce Steedman <bruce.steedman@gmail.com>
2015-01-14 16:51:10 +0000

    Add debugging console code

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -28,7 +28,9 @@ Padfoot::App.controllers :users do
 
   get :confirm, map: '/confirm/:id/:code' do
     redirect('/') unless @user = User[params[:id].to_i] # if user is nil
+    logger.info 'user found'
     redirect('/') unless @user.authenticate(params[:code].to_s)
+    logger.info 'user authenticated'
     render :confirm
   end
 
