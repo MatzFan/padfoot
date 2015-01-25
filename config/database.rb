832b157db8eab1be0f3f918f852a3179988df37d
Bruce Steedman <bruce.steedman@gmail.com>
2015-01-25 14:20:01 +0000

    Add config for staging db

--- config/database.rb
+++ config/database.rb
@@ -4,5 +4,6 @@ Sequel::Model.raise_on_save_failure = false # Do not throw exceptions on failure
 DB = case Padrino.env
   when :development then Sequel.connect("postgres://localhost/padfoot_development", loggers: [logger])
   when :production  then Sequel.connect(ENV['DATABASE_URL'], loggers: [logger])
+  when :staging     then Sequel.connect(ENV['DATABASE_URL'], loggers: [logger])
   when :test        then Sequel.connect("postgres://localhost/padfoot_test", loggers: [logger])
 end
