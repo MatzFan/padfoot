522691573183f8091017fed279b0e7e7faee0f7c
Bruce Steedman <bruce.steedman@gmail.com>
2015-01-30 18:07:12 +0000

    Add callback to hide first column & redraw

--- app/assets/javascripts/apps_table.js
+++ app/assets/javascripts/apps_table.js
@@ -1,14 +1,11 @@
-function drawTable(data) {
+function drawTable(data, callback) {
   var columns = data.columns;
   var appData = data.app_data;
   var thDivs = $(columns).map(function() {
     return "<th>"+this.title+"</th>";
   }).get().join('');
-  $("#table-placeholder").html("<table id='tbl' class='display table table-condensed table-striped table-bordered' data-toggle='table'><tfoot>" + thDivs + "</tfoot></table>");
+  $("#table-placeholder").html("<table id='tbl' class='table stripe table-bordered' data-toggle='table'><tfoot>" + thDivs + "</tfoot></table>");
   var table = $('#tbl').dataTable({
-    // "drawCallback": function() {
-    //   $('.obscure-while-loading').removeClass('obscure-while-loading'); // ensures html rendered after table draw
-    // },
     dom:         "rtiS", // add scroller, remove 'f' at front to hide filtering element
     scrollY:     440,
     scrollCollapse: true,
@@ -33,9 +30,9 @@ function drawTable(data) {
 
   // http://datatables.net/extensions/colvis/api
   var colvis = new $.fn.dataTable.ColVis( table );
-  $( colvis.button() ).insertAfter('#table-controls');
+  $( colvis.button() ).insertAfter('#tbl_length');
 
-  $('tfoot th').each( function () { // remove '#tbl', add '.dataTbales_scrollFoot' for scroller
+  $('.dataTables_scrollFoot tfoot th').each( function () { // remove '#tbl', add '.dataTbales_scrollFoot' for scroller
     var title = $('thead th').eq( $(this).index() ).text();  // remove '#tbl', add '.dataTables_scrollHead' for scroller
      $(this).html( '<input type="text" placeholder="'+title+'" />' );
   });
@@ -51,4 +48,5 @@ function drawTable(data) {
         .draw();
     });
   });
+  callback();
 }
