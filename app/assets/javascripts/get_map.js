1d7c391bc7facf902c71e85d5bb8c4f9efd7660b
Bruce Steedman <bruce.steedman@gmail.com>
2015-03-02 16:23:22 +0000

    Make map responsive and fit page width, change centre point

--- app/assets/javascripts/get_map.js
+++ app/assets/javascripts/get_map.js
@@ -5,7 +5,7 @@ function GetMap(locations, colours, letters, refs, descriptions) {
 
   var mapOptions = {
     credentials:'AutjcX00ethCZF2CQURexruteEwuFxvWJ6BVywEvyDv5BIaOO3vfhrH59O_rnLFd',
-    center: new Microsoft.Maps.Location(49.225, -2.135),
+    center: new Microsoft.Maps.Location(49.210, -2.135), // 49.225, -2.135 is Digimap center
     zoom: 13,
     mapTypeId: Microsoft.Maps.MapTypeId.aerial
   };
