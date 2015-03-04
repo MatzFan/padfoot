26fd6e915cb84179c65ff80a75cd38383b8d9b14
Bruce Steedman <bruce.steedman@gmail.com>
2015-03-04 17:13:59 +0000

    Modify scraper to return distinct parish nums & postcodes

--- lib/scrapers/location_scraper.rb
+++ lib/scrapers/location_scraper.rb
@@ -14,8 +14,9 @@ class LocationScraper
   RESULTS_FIELD = '_ctl0:cphContent:CompanyDetail:lpi_contact_address:lb_results'
   ADD_ID = '_ctl0_cphContent_CompanyDetail_lpi_contact_address_txt_address'
   ADDRESS_COUNT = '_ctl0_cphContent_CompanyDetail_lpi_contact_address_lbl_count'
+  PARISHES = %w(Grouville St.\ Brelade St.\ Clement St.\ Helier St.\ John St.\ Lawrence St.\ Martin St.\ Mary St.\ Ouen St.\ Peter St.\ Saviour Trinity)
 
-  attr_reader :agent
+  attr_reader :agent, :parishes
 
   def initialize(search_string, parish_num = 0)
     @search_string = search_string
@@ -23,6 +24,7 @@ class LocationScraper
     @agent = Mechanize.new
     @page_6 = page_6 # where the action is!
     @results_page = results_page
+    @form = @results_page.form(FORM)
   end
 
   def login_page
@@ -61,14 +63,25 @@ class LocationScraper
   end
 
   def addresses
-    (0...count).map { |i| address(i) }
+    (0...count).map { |i| postcode_and_parishify(address(i)) }
+  end
+
+  def postcode_and_parishify(arr)
+    [arr.join("<br/>"), parish(arr), (arr.last if arr.last =~ /^JE\d \d[A-Z]{2}$/)]
+  end
+
+  def parish(arr)
+    arr.reverse.each do |string|
+      index = PARISHES.index(string)
+      return (index + 1) if index
+    end
+    nil
   end
 
   def address(num)
-    form = @results_page.form(FORM)
-    form.field_with(name: RESULTS_FIELD).options[num].select
-    form.add_field!('__EVENTTARGET', RESULTS_FIELD.gsub(':', '$'))
-    @agent.submit(form).search('#' + ADD_ID).children.to_s.strip.gsub("\r",'')
+    @form.field_with(name: RESULTS_FIELD).options[num].select
+    @form.add_field!('__EVENTTARGET', RESULTS_FIELD.gsub(':', '$'))
+    @agent.submit(@form).search('#' + ADD_ID).children.to_s.strip.split("\r\n")
   end
 
   def data
--- spec/lib/scrapers/location_scraper_spec.rb
+++ spec/lib/scrapers/location_scraper_spec.rb
@@ -7,13 +7,15 @@ describe LocationScraper do
            69150001, 69213731, 69213734, 69385980, 69115920, 69213702,
            69213884, 69213951, 69387750]
 
-  LENORD = "Le Nord\nLa Rue de Crabbe\nSt. Mary\nJE3 3AD"
-  FOURNEAUX = [[69407485, "Fourneaux\nLe Clos Du Hambye\nLa Route de la " +
-                          "Hougue Bie\nSt. Saviour\nJE2 7BS"]]
+  LENORD = [69115918, ["Le Nord<br/>La Rue de Crabbe<br/>St. Mary<br/>JE3 3AD",
+             8, 'JE3 3AD']]
+  FOURNEAUX = [69407485, ['Fourneaux<br/>Le Clos Du Hambye<br/>La Route de la'+
+                ' Hougue Bie<br/>St. Saviour<br/>JE2 7BS', 11, 'JE2 7BS']]
 
   string = 'crabbe'
   parish_num = 2
   string_scraper = LocationScraper.new(string)
+  fourneaux = LocationScraper.new('fourneaux') # captures St. Brelade and St. Saviour
   parish_scraper = LocationScraper.new('fourneaux', 11) # 11 is St. Saviour
 
   context '#new' do
@@ -49,14 +51,38 @@ describe LocationScraper do
   end
 
   context '#address' do
-    it 'returns a single address at given index from search results' do
-      expect(string_scraper.address(3)).to eq(LENORD)
+    it 'returns an array representing the address string' do
+      expect(string_scraper.address(3)).to eq(LENORD.last[0].split('<br/>'))
+    end
+  end
+
+  context '#postcode_and_parishify' do
+    it 'returns a 3 element array: [html address string, parish number, postcode]' do
+      add = ['line1', 'line2', 'Trinity', 'JE1 1BJ']
+      arr = ['line1<br/>line2<br/>Trinity<br/>JE1 1BJ', 12, 'JE1 1BJ']
+      expect(string_scraper.postcode_and_parishify(add)).to eq(arr)
     end
   end
 
   context '#data' do
-    it 'returns a 2D array of uprn/address pairs' do
-      expect(parish_scraper.data).to eq(FOURNEAUX)
+    it 'returns a 2D array of uprn/address array pairs' do
+      expect(string_scraper.data.all? { |e| e.class == Array }).to be_truthy
+    end
+
+    it 'Address array element 0 is the html formatted address' do
+      expect(string_scraper.data[3].last[0]).to eq(LENORD.last[0])
+    end
+
+    it 'Address array element 1 is the parish number, or nil' do
+      expect(fourneaux.data.all? do |e|
+        [2, 11, nil].any? { |i| e.last[1] == i }
+      end).to be_truthy
+    end
+
+    it 'Address array element 2 is a valid postcode, or nil' do
+      expect(string_scraper.data.all? do |e|
+        e.last[2] =~ /^JE\d \d[A-Z]{2}$/ || e.last[2].nil?
+      end).to be_truthy
     end
   end
 
