258acc4f54b55d5d043c6271d2cf5e205a87a9f3
Bruce Steedman <bruce.steedman@gmail.com>
2015-05-23 11:54:23 +0100

    Get event callacks to work :smile:

--- app/controllers/stripe.rb
+++ app/controllers/stripe.rb
@@ -1,10 +1,24 @@
 Padfoot::App.controllers :stripe do
 
-  post :events, map: '/events', csrf_protection: false, provides: [:json] do
-    event_json = JSON.parse(request.body.read)
-    event = Stripe::Event.retrieve(event_json["id"])
-    logger.info event
-    status 200
+  before do # guarantees @event is a legitimate Stripe event
+    event_json = JSON.parse(request.body.read, symbolize_names: true)
+    begin
+      @event = Stripe::Event.retrieve(event_json[:id]) # callback
+    rescue Exception => e # rescues 404 error from Stripe - or missing id
+      logger.warn "Stripe says: #{e}"
+      halt 200
+    end
+  end
+
+  post :events, map: 'stripe_events', csrf_protection: false, provides: :json do
+    logger.info "Stripe event: #{@event.type}"
+    if @event.type == 'customer.subscription.created'
+      logger.info @event.data.object.status # should be 'active'
+      @user = DB[:users].first(stripe_cust_id: @event.data.object.customer)
+      # @user.update(subscription: true)
+      logger.info @user
+    end
+    return 200
   end
 
 end
