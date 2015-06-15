31f2c5eafb13d9360f266230ca10068efb04b3f8
Bruce Steedman <bruce.steedman@gmail.com>
2015-06-15 17:49:58 +0100

    Add unique constraint on Document model

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,8 +2,12 @@ class Document < Sequel::Model
 
   many_to_one :document_types, key: :name
   many_to_one :meetings
-
   many_to_many :planning_apps, left_key: :id, right_key: :app_ref
+
+  def validate
+    super
+    validates_unique :name # previously DB had duplicates..
+  end
 
   def before_save
     DocumentType.find_or_create(name: self.type) if self.type
