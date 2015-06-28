5f22c21c1fdb6301cea7970c42566cc280f310e5
Bruce Steedman <bruce.steedman@gmail.com>
2015-06-28 21:47:04 +0100

    Extend parser

--- lib/transaction_parser.rb
+++ lib/transaction_parser.rb
@@ -2,16 +2,65 @@ require 'mechanize'
 
 class TransactionParser
 
+  HEADER_ROWS = %w(Surname: Maiden\ Name: Summary\ Details: Index: Doc\ Type: Book/Page: Role:)
+  PARTY_ROWS = %w(Role Surname/Corporate\ Name Maiden\ Name Forename Extended\ Text)
+  PROPERTY_ROWS = %w(Prop\ Id(s) Parish(es) Address(es))
+
   def initialize(file)
     @file = file
     @agent = Mechanize.new
     @page = page
+    @tables = tables
+    @header_table_rows = header_table_rows
+    @party_table_rows = party_table_rows
+    @property_table_rows = property_table_rows
+    fail unless verify_structure
+  end
+
+  def verify_structure
+    HEADER_ROWS == @header_table_rows.map { |r| r.children.children[0].text } &&
+    PARTY_ROWS == @party_table_rows.first.children.children.map(&:text) &&
+    PROPERTY_ROWS == @property_table_rows.first.children.children.map(&:text)
   end
 
   def page
     @agent.get(@file)
   end
 
+  def tables
+    @page.search('table')
+  end
 
+  def header_table_rows
+    @tables[2].search('tr')
+  end
+
+  def doc_num
+    @header_table_rows[3].children.children.last.text.to_i
+  end
+
+  def doc_type
+    @header_table_rows[4].children.children.last.text
+  end
+
+  def doc_type
+    @header_table_rows[4].children.children.last.text
+  end
+
+  def party_table_rows
+    @tables[5].search('tr')
+  end
+
+  def parties
+    @party_table_rows[1..-1].map { |r| r.children.children.map(&:text) }
+  end
+
+  def property_table_rows
+    @tables[7].search('tr')
+  end
+
+  def properties
+    @property_table_rows[1..-1].map { |r| r.children.children.map(&:text) }
+  end
 
 end
--- spec/lib/transaction_parser_spec.rb
+++ spec/lib/transaction_parser_spec.rb
@@ -2,6 +2,7 @@ describe TransactionParser do
 
   uri = URI.join('file:///', "#{Padrino.root}/spec/fixtures/single_page_trans.html")
   let(:parser) { TransactionParser.new(uri) }
+  let(:single_property) { ["69117812", "St. Helier", "Almeda, 7 La Rue de Pod棚tre, "] }
 
   context '#new' do
     it 'should return an instance of the class' do
@@ -15,6 +16,55 @@ describe TransactionParser do
     end
   end
 
+  context '#tables' do
+    it 'should return 8 tables for a page' do
+      expect(parser.tables.count).to eq(8)
+    end
+  end
 
+  context 'header data' do
+    context '#header_table_rows' do
+      it 'should have 7 elements' do
+        expect(parser.header_table_rows.size).to eq(7)
+      end
+    end
+
+    context '#doc_num' do
+      it "should return 1" do
+        expect(parser.doc_num).to eq(1)
+      end
+    end
+
+    context '#doc_type' do
+      it "should return 'Realty - Sale'" do
+        expect(parser.doc_type).to eq('Realty - Sale')
+      end
+    end
+
+  end
+
+  context '#party_table_rows' do
+    it 'should have 6 elements' do
+      expect(parser.party_table_rows.size).to eq(6)
+    end
+  end
+
+  context '#parties' do
+    it "should return 5 parties" do
+      expect(parser.parties.count).to eq(5)
+    end
+  end
+
+  context '#property_table_rows' do
+    it 'should have 2 elements' do
+      expect(parser.property_table_rows.size).to eq(2)
+    end
+  end
+
+  context '#properties' do
+    it "returns a 2D 'property' array with a single element (one property)" do
+      expect(parser.properties.count).to eq(1)
+    end
+  end
 
 end
