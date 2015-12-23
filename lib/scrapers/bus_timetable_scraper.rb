08b4a97a3f2beeed89045431609de986f7fdd006
Bruce Steedman <bruce.steedman@gmail.com>
2015-12-23 00:10:08 +0000

    add methods

--- lib/scrapers/bus_timetable_scraper.rb
+++ lib/scrapers/bus_timetable_scraper.rb
@@ -2,16 +2,17 @@ require 'mechanize'
 
 class BusTimetableScraper
 
-  ROOT = 'http://www.libertybus.je/'
-  TIMETABLES_PAGE = 'routes_times/timetables'
-  COORDS = [:Latitude, :Longitude]
+  ROOT = 'http://www.libertybus.je'
+  TIMETABLES_PAGE = '/routes_times/timetables'
 
   def initialize
     @agent = Mechanize.new
     @timetables_page = @agent.get(ROOT + TIMETABLES_PAGE)
+    @routes = routes
+    @links = links
   end
 
-  def title
+  def main_title
     @timetables_page.search("//div[@id='col-default-editor']/h2").text
   end
 
@@ -19,12 +20,28 @@ class BusTimetableScraper
     @timetables_page.search("//ul[@id='main-timetable-list']/li/a")
   end
 
-  def route_links
-    routes.map { |e| e.attribute('href').value.gsub('TRUE', 'FALSE') }
+  def links
+    @routes.map { |e| e.attribute('href').value.gsub('TRUE', 'FALSE') }
   end
 
-   def route_names
-    routes.map { |e| e.children[3].text }
+  def names
+    @routes.map { |e| e.children[3].text }
+  end
+
+  def timetable_page(index)
+    @agent.get(ROOT + @links[index])
+  end
+
+  def timetable_titles(page)
+    page.search("//div[@class='t-table']/h2").children.map(&:text).map &:strip
+  end
+
+  def header_tables(page)
+    page.search("//div[@class='t-table']/table")
+  end
+
+  def times_tables(page)
+    page.search("//div[@class='t-table']/div[@class='scroll']/table")
   end
 
 end
--- spec/lib/scrapers/bus_timetable_scraper_spec.rb
+++ spec/lib/scrapers/bus_timetable_scraper_spec.rb
@@ -1,6 +1,14 @@
 describe BusTimetableScraper do
 
   let(:scraper) { BusTimetableScraper.new }
+  let(:route_1) { scraper.timetable_page(0) }
+  let(:titles) { ["Liberation Station - Gorey Pier - Outbound - Monday-Friday",
+                  "Gorey Pier - Liberation Station  - Inbound - Monday-Friday",
+                  "Liberation Station - Gorey Pier - Outbound - Saturday",
+                  "Gorey Pier - Liberation Station  - Inbound - Saturday",
+                  "Liberation Station - Gorey Pier - Outbound - Sunday",
+                  "Gorey Pier - Liberation Station  - Inbound - Sunday"]
+                }
 
   context '#new' do
     it 'returns an instance of the class' do
@@ -8,9 +16,9 @@ describe BusTimetableScraper do
     end
   end
 
-  context '#title' do
+  context '#main_title' do
     it 'returns the title of the latest timetable' do
-      expect(scraper.title).to eq 'Winter Timetable, updated from 12 October 2015'
+      expect(scraper.main_title).to eq 'Winter Timetable, updated from 12 October 2015'
     end
   end
 
@@ -20,15 +28,39 @@ describe BusTimetableScraper do
     end
   end
 
-  context '#route_links' do
+  context '#links' do
     it 'returns an array of links to each timetable' do
-      expect(scraper.route_links.first).to eq '/routes_times/timetables/1/FALSE'
+      expect(scraper.links.first).to eq '/routes_times/timetables/1/FALSE'
     end
   end
 
-  context '#route_names' do
+  context '#names' do
     it 'returns an array of route names' do
-      expect(scraper.route_names.first).to eq 'Liberation Station - Gorey Pier'
+      expect(scraper.names.first).to eq 'Liberation Station - Gorey Pier'
+    end
+  end
+
+  context '#timetable_page' do
+    it 'returns a Mechanize::Page for the given route index' do
+      expect(scraper.timetable_page(0).title).to eq 'Liberty Bus - Timetables'
+    end
+  end
+
+  context '#timetable_titles' do
+    it 'returns the title of the given timetable' do
+      expect(scraper.timetable_titles(route_1)).to eq titles
+    end
+  end
+
+  context '#headers_tables' do
+    it 'returns the headers tables for the given timetable' do
+      expect(scraper.header_tables(route_1).count).to eq 6
+    end
+  end
+
+  context '#times_tables' do
+    it 'returns the times tables for the given timetable' do
+      expect(scraper.times_tables(route_1).count).to eq 6
     end
   end
 
