76dbbdd8b3a7604b9a717d59ca7bbfaaa336ec07
Bruce Steedman <bruce.steedman@gmail.com>
2016-03-04 17:26:39 +0000

    remove curb gem dependancy

--- lib/scrapers/address_scraper.rb
+++ lib/scrapers/address_scraper.rb
@@ -1,9 +1,10 @@
-require 'curb'
+require 'open-uri'
 
+# scrapes Jersey postal addresses
 class AddressScraper
-
-  PC_URL = "http://www.jerseypost.com/tools/postcode-address-finder/"
-  CLICK = "Click here for address information"
+  BASEURL = 'http://www.jerseypost.com/tools/postcode-address-finder/?'.freeze
+  CLICK = 'Click here for address information'.freeze
+  FIND = 'Find+an+Address'.freeze
 
   attr_reader :addresses, :address_arrays
 
@@ -15,15 +16,15 @@ class AddressScraper
 
   def raw_adds
     return [] if num_addresses == 0
-    (0..(@num_addresses/100.0).ceil).inject([]) do |memo, i|
-      memo << adds_on_page(i).map { |s| s.split(', ').map(&:strip) } # note comma + space
+    (0..(@num_addresses / 100.0).ceil).inject([]) do |memo, i|
+      memo << adds_on_page(i).map { |s| s.split(', ').map(&:strip) } # ', '
     end.first
   end
 
   def adds_on_page(page_num)
-    address_block = source(page_num).match(/#{CLICK}">(.*)<\/select>/mu)[1]
-    adds = address_block.split(/<\/?option>/).map(&:strip)
-    adds.values_at(* adds.each_index.select { |i| i.odd? }) # need to select only odd values, as even ones are ""
+    address_block = source(page_num).match(%r{#{CLICK}">(.*)</select>}mu)[1]
+    adds = address_block.split(%r{</?option>}).map(&:strip)
+    adds.values_at(* adds.each_index.select(&:odd?)) # even ones are ""
   end
 
   def num_addresses
@@ -33,11 +34,8 @@ class AddressScraper
   end
 
   def source(page_num)
-    Curl.get(PC_URL, {
-      string: @search_string,
-      search: "Find+an+Address",
-      first: page_num
-    }).body_str.force_encoding(Encoding::UTF_8)
+    url = "#{BASEURL}&string=#{@search_string}&search=#{FIND}&first=#{page_num}"
+    open(url).read.force_encoding(Encoding::UTF_8)
   end
 
   def addresses # remove 'JERSEY', separate road, parish and postcode
@@ -53,7 +51,8 @@ class AddressScraper
   end
 
   def htmlify(arr) # arr is from 4 to 9 elements
-    s, n = arr[0], (arr.length - 4)
+    s = arr[0]
+    n = arr.length - 4
     (1..n).each { |i| s += int?(arr[i - 1]) ? ' ' + arr[i] : '<br/>' + arr[i] }
     s + (arr[-3].empty? ? '' : '<br/>' + arr[-3]) + '<br/>' + arr[-1] # add parish and postcode
   end
@@ -61,7 +60,6 @@ class AddressScraper
   def int?(string)
     string.to_i.to_s == string
   end
-
 end
 
 # s = AddressScraper.new('baal')
