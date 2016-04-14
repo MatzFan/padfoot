c678aec95636c6d6b893b9201b59ebc1b377f09e
Bruce Steedman <bruce.steedman@gmail.com>
2016-04-14 12:10:45 +0100

    fix planning app table rendering

--- app/controllers/planning_app.rb
+++ app/controllers/planning_app.rb
@@ -4,15 +4,15 @@ Padfoot::App.controllers :planning_app do
   get :index, map: 'applications/index', provides: [:html, :json] do
     refs = params[:refs] # a comma separated string of app_refs
     apps = refs ? apps_ordered(refs.split(',')) : all_apps_ordered # helpers
-    # @titles = PlanningApp::TABLE_COLS.map { |c| c.to_s.split('_').last.capitalize }
-    @titles = PlanningApp::TABLE_TITLES
-    @app_arr = apps.select_map(PlanningApp::TABLE_COLS) # 2D array
-    @app_arr.first.inspect
-    # classes_to_add = PlanningApp::TABLE_COLS.map { |c| 'long-text' if c == :app_description }
-    # # wrap txt in <div>s and add class to app_description for css formatting
-    # @app_arr.map! { |app| div_wrap_strings_in(app, classes_to_add) }
-    # json = { columns: @titles.map { |t| { title: t } }, app_data: @app_arr }.to_json
-    # content_type == :json ? json : render(:index) # assume html
+    @tits = PlanningApp::TABLE_TITLES
+    @apps = apps.select_map(PlanningApp::TABLE_COLS) # 2D array
+    classes_to_add = PlanningApp::TABLE_COLS.map do |c|
+      'long-text' if c == :app_description
+    end
+    # wrap txt in <div>s and add class to app_description for css formatting
+    @apps.map! { |app| div_wrap_strings_in(app, classes_to_add) }
+    json = { columns: @tits.map { |t| { title: t } }, app_data: @apps }.to_json
+    content_type == :json ? json : render(:index) # assume html
   end
 
   post :index, map: 'applications/index' do
@@ -37,13 +37,16 @@ Padfoot::App.controllers :planning_app do
   end
 
   get :within_circle, map: 'applications/within_circle', provides: :json do
-    lat, long, radius = params[:lat], params[:long], params[:radius]
+    lat = params[:lat]
+    long = params[:long]
+    radius = params[:radius]
     apps = PlanningApp.within_circle(lat.to_f, long.to_f, radius.to_f)
     PlanningApp.pin_data_hash(apps).to_json
   end
 
   get :within_polygon, map: 'applications/within_polygon', provides: :json do
-    lats, longs = params[:lats], params[:longs]
+    lats = params[:lats]
+    longs = params[:longs]
     apps = PlanningApp.within_polygon(PlanningApp.transform(lats, longs))
     PlanningApp.pin_data_hash(apps).to_json
   end
