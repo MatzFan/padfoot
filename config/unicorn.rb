cca087d83894a5d476d3a62212479b217cef69a1
Bruce Steedman <bruce.steedman@gmail.com>
2016-08-12 15:23:29 +0100

    change WEB_CONCURRENCY to 1

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,8 @@
-worker_processes Integer(ENV['WEB_CONCURRENCY'] || 2) # default is 3
+worker_processes Integer(ENV['WEB_CONCURRENCY'] || 1) # default is 3
 timeout 60
 preload_app true
 
-before_fork do |server, worker|
-
+before_fork do |_server, _worker|
   Signal.trap 'TERM' do
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
